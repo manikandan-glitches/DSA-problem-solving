1749a3824faf57c72d7e2eec14686cba6bad56d3
Manikandan <mani007893@gmail.com>
2026-02-17 11:20:08 +0530

    Update longest-commen-prefix.cpp

--- string/longest-commen-prefix.cpp
+++ string/longest-commen-prefix.cpp
@@ -1,1 +1,21 @@
+/*simple sorting concept... another algo also that was binery search*/
 
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string pref="";
+        int a=0;
+        int n=strs[0].length();
+        int m=strs.size();
+        sort(strs.begin(),strs.end());
+        for(int i=0;i<n;i++)
+            {
+               if(strs[0][i]!=strs[m-1][i])
+               {
+                   return pref;
+               }
+                pref+=strs[0][i];
+            }
+        return pref;
+    }
+};
